c9c6fccea433650a18fd2593ba79949733f07973
ojpd <ojpd@users.noreply.github.com>
2021-06-29 14:11:04 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.ac9881d94695bc0f7123bc50919667d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd0e7bec7110a1fe5179f3fb34fda1e",
+    "url": "https://ojpd.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "27cacccea548a847d6cd",
+    "url": "https://ojpd.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "41ffedde223ca7165a03",
+    "url": "https://ojpd.github.io/task-management-frontend/static/js/2.baf5c498.chunk.js"
+  },
+  {
+    "revision": "27cacccea548a847d6cd",
+    "url": "https://ojpd.github.io/task-management-frontend/static/js/main.1f72a580.chunk.js"
+  },
+  {
+    "revision": "fa665b0bb0649f68c1a3",
+    "url": "https://ojpd.github.io/task-management-frontend/static/js/runtime~main.eadcd5c6.js"
+  }
+]);
